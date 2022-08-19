bb088e20efaacf9114ed5f026714b99d0817f0d3
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-08-19 12:47:03 -0400

    Learning the basics - methods, arguments, etc.

---
+++ Section 2_Ruby-basics/section2.rb
@@ -0,0 +1,16 @@
+# Some copied from repelit.com
+# Run using 'ruby <filename>' -- in this case 'section2.rb'
+
+# print "Hello World\n"
+# puts "Hello World"
+# p "Hello World"
+
+#greeting = "Hello World"
+# puts greeting
+
+# Defining a method that takes in an argument #
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello "Hello World Ruby is great!"
